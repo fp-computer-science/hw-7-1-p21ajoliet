c8ba939dd964a73eb607ea93b8b10b09a5d026de
Alex Joliet <p21ajoliet@fairfieldprep.org>
2020-11-08 17:26:04 -0500

    HW 7-1 done

---
+++ hw7-1.py
@@ -0,0 +1,31 @@
+#Author: ALJ (AMDG) 11/8/20
+
+#Question 1
+print('python'[2])
+
+#Question 2
+print('Strings are simply a sequence of characters.'[30:32])
+
+#Question 3
+print(len('excellent'))
+
+#Question 4
+print(bool('pineapple'.find('apple')))
+
+#Question 5
+print(bool('pear' > 'Peach'))
+
+#Question 6
+print('x' * 100)
+
+#Question 7
+print(type(str(int(54321))))
+
+#Question 8
+print(bool(('pineapple').find('p')))
+
+#Question 9
+print('\"Livin\' On a Prayer\" is my friend\'s favorite song.')
+
+#Question 10
+print(len('\"Livin\' On a Prayer\" is my friend\'s favorite song.'))
